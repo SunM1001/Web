b0b7776c48f5261a183de81de6dc9b52cf2ca352
ysm1001 <108646818+SunM1001@users.noreply.github.com>
2023-03-27 13:19:49 +0900

    ㅁㄴㅇㄹ
    ㅁㄴㅇㄹ

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
-$(function(){
-    $('div').click(function(){
-        $(this).css('webkit-animation-name', 'duration')
-    })
+const btn = document.querySelector('button');
+const box = document.querySelector('story');
+
+// div 클릭 시 act 클래스 토글
+btn.addEventListener('click', () => {
+  box.classList.toggle('act');
 })
