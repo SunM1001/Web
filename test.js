fdd8ed8b494ae3aa7edc0af3ffc34333e05f7959
ysm1001 <108646818+SunM1001@users.noreply.github.com>
2023-04-20 15:47:28 +0900

    ㅁㄴㅇㄹ
    ㅁㄴㅇㄹ

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var ani1 = document.querySelector('.story_ani');
 var ani2 = document.querySelector('.form2');
 var story = document.querySelector('.story');
+var loadanimation1 = document.querySelector('.form1');
+var loadanimation2 = document.querySelector('.form2');
 
 
 ani1.addEventListener("mouseover", function() {
@@ -13,4 +15,9 @@ ani1.addEventListener("mouseout", function() {
   ani2.style.transform = 'translateY(-50px)';
   ani2.style.transition = 'all 1s';
   ani2.style.opacity = '1';
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  loadanimation1.classList.add('animate');
 });
