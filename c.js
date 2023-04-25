aed1d493670f119e77d7938e425b88ead08c4fb6
ysm1001 <108646818+SunM1001@users.noreply.github.com>
2023-04-25 16:01:34 +0900

    ㅁㄴㅇㄹ
    ㅁㄴㅇㄹ

--- c.js
+++ c.js
@@ -1,27 +1,50 @@
-const container = document.querySelector('#image-container');
-let currentImage = 0;
+// 변수 선언
+var container = document.querySelector(".image-container");
+var images = container.querySelectorAll("img");
 
-container.addEventListener('wheel', (event) => {
-  event.preventDefault();
+var currentX = 0;
+var dragStartX = 0;
+var dragEndX = 0;
+var threshold = 100;
 
-  const delta = Math.sign(event.deltaY);
-  const images = container.querySelectorAll('img');
-  const maxImage = images.length - 1;
+// 드래그 이벤트 핸들러 등록
+container.addEventListener("mousedown", dragStart);
+container.addEventListener("mousemove", dragMove);
+container.addEventListener("mouseup", dragEnd);
+container.addEventListener("mouseleave", dragEnd);
+container.addEventListener('wheel', wheelMove);
 
-  currentImage += delta;
+// 드래그 시작 시 호출되는 함수
+function dragStart(e) {
+  dragStartX = e.pageX - container.offsetLeft;
+  currentX = container.scrollLeft;
+}
 
-  if (currentImage > maxImage) {
-    currentImage = maxImage;
-  } else if (currentImage < 0) {
-    currentImage = 0;
+// 드래그 중일 때 호출되는 함수
+function dragMove(e) {
+  if (dragStartX) {
+    e.preventDefault();
+    dragEndX = e.pageX - container.offsetLeft;
+    var distance = dragEndX - dragStartX;
+    container.scrollLeft = currentX - distance;
   }
+}
 
-  images.forEach((image, index) => {
-    if (index === currentImage) {
-      image.style.display = 'block';
-    } else {
-      image.style.display = 'none';
-    }
-  });
-});
+// 드래그 종료 시 호출되는 함수
+function dragEnd(e) {
+  var distance = dragEndX - dragStartX;
+  e.preventDefault();
+  container.removeEventListener('mousemove', dragMove);
+}
 
+
+function wheelMove(e) {
+  e.preventDefault();
+  var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+  container.scrollLeft -= (delta * 30);
+  
+  // 마우스 휠 이벤트 발생 후 container 영역을 벗어나면 스크롤 초기화 방지
+  if (e.pageX < container.offsetLeft || e.pageX > container.offsetLeft + container.offsetWidth) {
+    container.scrollLeft += (delta * 30);
+  }
+}
